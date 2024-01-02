7bb5e66f71ad39c961ed3c02eefdabff743a296c
Jalal Amourgha <myproo100@gmail.com>
2024-01-02 18:11:05 +0100

    adding review section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 let switcher = document.querySelectorAll(".menu-bar .row div");
 let divs = document.querySelectorAll(".menu-items .menu-box");
 
+// This function for navbar background color
+window.addEventListener("scroll", function () {
+  var navbar = document.querySelector("nav");
+  navbar.classList.toggle("sticky", window.scrollY > 0);
+});
+
+// Filter menu bar
 switcher.forEach((e) => {
   e.addEventListener("click", removeActive);
   e.addEventListener("click", manageImgs);
@@ -13,6 +20,7 @@ function removeActive() {
   });
 }
 
+// This function for showning the menu items that selected
 function manageImgs() {
   divs.forEach((div) => {
     div.classList.add("hide");
@@ -23,13 +31,14 @@ function manageImgs() {
   });
 }
 
+// This function for chnage label color when input clicked
 function changeLabelColor(input) {
   var selectedInput = document.querySelector(`label[for="${input}"]`);
   selectedInput.style.color = "#e3292a";
 }
 
+// This function for remove label color when input unclicked
 function resetLabelColor(input) {
   var label = document.querySelector(`label[for="${input}"]`);
-
   label.style.color = "#999";
 }
